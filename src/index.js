70715ad5b61c004676f64c7375f047fddb4117a3
JustF <folkerthoogenraad@hotmail.nl>
2019-11-27 17:01:37 +0100

    Created the template ready to go

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import "./js/main";
+import "./scss/app.scss";
---
+++ webpack.production.config.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  mode: "production",
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/html/index.html'
+   })
+  ]
+};
+
+module.exports = config;

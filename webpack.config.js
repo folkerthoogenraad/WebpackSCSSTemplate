d13f28d1ffc1a0bd3801fbfeb7776eb8c7fd3cd2
JustF <folkerthoogenraad@hotmail.nl>
2020-01-11 09:19:56 +0100

    Added some more to the template

--- webpack.config.js
+++ webpack.config.js
@@ -25,13 +25,63 @@ const config = {
           'css-loader',
           'sass-loader'
         ]
-      }
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        use: [
+          {
+            loader:'file-loader',
+            options: {
+              name: "[name].[ext]",
+              outputPath:"assets/img/",
+              publicPath:"assets/img/",
+              esModule: false,
+            }
+          }
+        ],
+      },
+      {
+        test: /\.(mp4|webm)$/i,
+        use: [
+          {
+            loader:'file-loader',
+            options: {
+              name: "[name].[ext]",
+              outputPath:"assets/video/",
+              publicPath:"assets/video/",
+              esModule: false,
+            }
+          }
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader:'file-loader',
+            options: {
+              name: "[name].[ext]",
+              outputPath:"assets/fonts/",
+              publicPath:"assets/fonts/"
+            }
+          }
+        ],
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            attrs: ['img:src', 'video:src']
+          }
+        }
+      },      
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/html/index.html'
-   })
+    }),
   ]
 };
 
